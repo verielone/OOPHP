9189c52c8f2886a9192edf4a2fafbd00e37f107e
verielone <savendavenda@gmail.com>
2021-10-22 23:03:04 +0700

    mengganti beberapa nama file & menambahkan file object-type

---
+++ object-type.php
@@ -0,0 +1,41 @@
+<?php
+
+// jualan produk
+// komik, game
+
+class Produk {
+    public $judul,
+           $penulis,
+           $penerbit,
+           $harga;
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = "harga") {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function setLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+}
+
+class CetakInfoProduk {
+    public function cetak($produk){
+        $str = "{$produk->judul} | {$produk->setLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
+
+$produk2 = new Produk("Doraemon", "Savenda", "Shonen", 29000);
+
+echo "Komik : " . $produk1->setLabel();
+echo "<br>";
+echo "Komik : " . $produk2->setLabel();
+echo "<br>";
+
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak($produk1);
