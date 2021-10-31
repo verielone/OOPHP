9c5aede89de38dfb0a337e7fac7c9a2514292622
verielone <savendavenda@gmail.com>
2021-10-31 17:21:06 +0700

    update file setter-getter.php

--- setter-getter.php
+++ setter-getter.php
@@ -4,14 +4,11 @@
 // komik, game
 
 class Produk {
-    public $judul,
+    private $judul,
            $penulis,
-           $penerbit;
-
-    protected $diskon = 0;
-
-    private $harga;
-
+           $penerbit,
+           $harga,
+           $diskon;
 
     public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = "harga") {
         $this->judul = $judul;
@@ -20,8 +17,48 @@ class Produk {
         $this->harga = $harga;
     }
 
+    public function setJudul( $judul ) {
+        $this->judul = $judul;
+        return $this->judul;
+    }
+
+    public function getJudul() {
+        return $this->judul;
+    }
+
+    public function setPenulis( $penulis ) {
+        $this->penulis = $penulis;
+        return $this->penulis;
+    }
+
+    public function getPenulis() {
+        return $this->penulis;
+    }
+
+    public function setPenerbit( $penerbit ) {
+        $this->penerbit = $penerbit;
+        return $this->penerbit;
+    }
+
+    public function getPenerbit() {
+        return $this->penerbit;
+    }
+
+    public function setHarga( $harga ) {
+        $this->harga = $harga;
+        return $this->harga;
+    }
+
     public function getHarga() {
         return $this->harga - ( $this->harga * $this->diskon / 100);
+    }
+
+    public function setDiskon( $diskon ) {
+        $this->diskon = $diskon;
+    }
+
+    public function getDiskon() {
+        return $this->diskon;
     }
 
     public function setLabel() {
@@ -58,10 +95,6 @@ class Game extends Produk{
         $this->waktuMain = $waktuMain;
     }
 
-    public function setDiskon( $diskon ) {
-        $this->diskon = $diskon;
-    }
-
     public function getInfoProduk() {
         $str = "Game : ". parent::getInfoProduk() ." ~ {$this->waktuMain} Jam";
         return $str;
@@ -86,3 +119,7 @@ echo "<hr>";
 
 $produk2->setDiskon(10);
 echo $produk2->getHarga();
+echo "<hr>";
+
+$produk1->setJudul("JudulBaru");
+echo $produk1->getPenerbit();
